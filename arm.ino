fc5b0ef1070be390efbe6eed08353070c1a2537d
benchampe-pixel <benchampe@gmail.com>
2025-11-04 11:31:05 -0700

    Add files via upload
    Adding arduino files

---
+++ arm.ino
@@ -0,0 +1,36 @@
+#include <ESP32Servo.h>
+
+Servo horizontalServo, shoulderServo, elbowServo, wristServo, magnetServo;
+float horizontalValue = 0, shoulderValue = 105, elbowValue = 90, wristValue = 90, magnetValue = 90;
+
+void setup() {
+  Serial.begin(115200);
+  horizontalServo.attach(13);   // change pins as needed
+  shoulderServo.attach(14);
+  elbowServo.attach(15);
+  wristServo.attach(16);
+  magnetServo.attach(17);
+}
+
+void loop() {
+  if (Serial.available()) {
+    String data = Serial.readStringUntil('\n');
+    float s1, s2, s3, s4, s5;
+    
+    if (sscanf(data.c_str(), "%f,%f,%f,%f,%f", &s1, &s2, &s3, &s4, &s5) == 5) {
+      horizontalValue = s1;  shoulderValue = s2;  elbowValue = s3, wristValue = s4, magnetValue = s5;
+
+      horizontalValue = constrain(horizontalValue, 0, 180);
+      shoulderValue = constrain(shoulderValue, 0, 180);
+      elbowValue = constrain(elbowValue, 0, 180);
+      wristValue = constrain(wristValue, 0, 180);
+      magnetValue = constrain(magnetValue, 0, 180);
+    }
+
+    horizontalServo.write(horizontalValue);
+    shoulderServo.write(shoulderValue);
+    elbowServo.write(elbowValue);
+    wristServo.write(wristValue);
+    magnetServo.write(magnetValue);
+  }
+}
